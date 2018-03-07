73adfa3c48454442d2060ead5d0015b4355f6cae
Kevin Almanzar <kevinalmanzar8@gmail.com>
2018-03-06 20:40:45 -0500

    Added Spinner to LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
   onEmailChange(text) {
@@ -18,16 +18,16 @@ class LoginForm extends Component {
     this.props.loginUser({ email, password });
   }
 
-  renderError() {
-    if (this.props.error) {
-      return (
-        <View style={{ backgroundColor: 'white' }}>
-          <Text style={styles.errorTextStyle}>
-            {this.props.error}
-          </Text> 
-        </View>
-      );
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size='large' />;
     }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Login
+      </Button>
+    )
   }
 
   render() {
@@ -52,12 +52,12 @@ class LoginForm extends Component {
           />
         </CardSection>
 
-        {this.renderError()}
+        <Text style={styles.errorTextStyle}>
+          {this.props.error}
+        </Text>
 
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Login
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
@@ -72,12 +72,10 @@ const styles = {
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    email: state.auth.email,
-    password: state.auth.password,
-    error: state.auth.error
-  };
+const mapStateToProps = ({ auth }) => {
+  const { email, password, error, loading } = auth;
+
+  return { email, password, error, loading};
 };
 
 export default connect(mapStateToProps, { 
